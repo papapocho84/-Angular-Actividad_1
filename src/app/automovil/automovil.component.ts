019c3a7a05a5f4d2bd69006373911424f15a4da9
B612 <64872598+papapocho84@users.noreply.github.com>
2024-06-09 01:43:13 -0500

    ultimos cambios

--- src/app/automovil/automovil.component.ts
+++ src/app/automovil/automovil.component.ts
@@ -12,7 +12,7 @@ export class AutomovilComponent {
   tipo = 'Super Car'
   version = 16.4
   paisDeOrigen = 'Italia'
-  descripcion = 'The heart and soul of the Veyron is the 8.0-liter W16 engine with four turbochargers, which initially generated 1,001 PS, and an incredible 1,200 PS in later models. The 16-cylinder mid-engine is 710 mm in length, no longer than a conventional V12 engine, and weighs just 490 kg due to its lightweight construction.'
+  descripcion = 'The heart and soul of the Veyron is the 8.0-liter W16 engine with four turbochargers, which initially generated 1,001 PS'
   ulImagen = 'https://www.thesupercarblog.com/wp-content/uploads/2021/01/Used-2014-Bugatti-Veyron-164-Grand-Sport-Vitesse-1.jpg'
 
 }
